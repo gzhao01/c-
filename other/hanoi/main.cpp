370453ac84bc878c0ca24779222e386f4d236ffd
gzhao01 <1079756007@qq.com>
2019-11-11 22:18:13 +0800

    c++ reposisory

--- other/hanoi/main.cpp
+++ other/hanoi/main.cpp
@@ -2,8 +2,25 @@
 
 using namespace std;
 
+void hanoi(int n,int from,int tmp,int to);
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    hanoi(3,1,2,3);
 }
+void hanoi(int n,int from,int tmp,int to){
+    if(n==0){
+        return;
+    }
+    else if(n==1){
+        std::cout<<n<<"->"<<to<<"\n";
+    }
+    else{
+        //move n-1 hanoi to tmp
+        hanoi(n-1,from,to,tmp);
+        //move n to target
+        std::cout<<n<<"->"<<to<<"\n";
+        //move n-1 from tmp to target
+        hanoi(n-1,tmp,from,to);
+    }
+}
+
